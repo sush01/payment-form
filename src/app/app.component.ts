b94fa8ece4cf016cf29b182bb9d345e99e671a44
sush01 <kandel.sushila242@gmail.com>
2023-07-05 00:53:06 +0300

    payment form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pay';
+  
+  name: string = ``;
+  date: string = ``;
+  amount: number = 0 ;
+  
+  onNameChange($event: any){
+    this.name = $event?.target?.value;
+    console.log($event);
+
+}
+
+onDateChange($event: any){
+  this.date = $event?.target?.value;
+}
+
+onAmountChange($event: any){
+  const amt = $event?.target?.value;
+  this.amount = parseFloat(amt);
+}
 }
